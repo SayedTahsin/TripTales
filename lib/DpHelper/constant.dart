c42b45bb7db9ea71368cce79c1ba0e4d7c16f09b
Sayed Tashinoor Rahman <75539185+SayedTahsin@users.noreply.github.com>
2023-07-30 12:37:35 +0600

    Update constant.dart

--- lib/DpHelper/constant.dart
+++ lib/DpHelper/constant.dart
@@ -1,4 +1,4 @@
-const MONGO_CONN_URL = "mongodb+srv://SayedTahsin:tahsin99.@clusternew.4fivpok.mongodb.net/TripTales?retryWrites=true&w=majority";
+const MONGO_CONN_URL = "mongodb+srv://Username:pass@clusternew.4fivpok.mongodb.net/TripTales?retryWrites=true&w=majority";
 const USER_COLLECTION = "Posts";
 
 
